0a0489529686f98e384ba79c7fe353b3b78713eb
Sunghoman <ksh_1035@naver.com>
2022-10-28 17:13:09 +0900

    BoardPage 라우팅 및 컴포넌트 생성

--- src/Shared/Router.js
+++ src/Shared/Router.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Main } from "../components/Main/Main";
+import { MainPage } from "../Pages/MainPage";
+import { BoardPage } from "../Pages/BoardPage";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={ <Main/> } />
+        <Route exact path="/" element={ <MainPage/> } />
+        <Route exact path="/board" element={ <BoardPage/> } />
       </Routes>
     </BrowserRouter>
   )
