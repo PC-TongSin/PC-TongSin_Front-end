4b11d886dfb5c9927f9daf6327a41004366aa6e8
cho_jae_sin <1005jsc@naver.com>
2022-10-28 23:30:34 +0900

    업로드 라우터 생성

---
+++ src/Pages/UploadPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header/Header';
+
+const UploadPage = ({}) => {
+  return (
+    <>
+      <Header />
+      <Layout>
+        <h1>hihi</h1>
+      </Layout>
+    </>
+  );
+};
+export default UploadPage;
+
+const Layout = styled.div`
+  width: 100%;
+  padding: 0 20%;
+`;
