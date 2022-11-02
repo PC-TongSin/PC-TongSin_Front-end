cdc1a4d89f716e4eaeffcbbe71727a6cc8374e2e
Sunghoman <ksh_1035@naver.com>
2022-11-02 23:29:36 +0900

    ttt

--- src/components/Chat/ChatRoom/ChatRoom.jsx
+++ src/components/Chat/ChatRoom/ChatRoom.jsx
@@ -12,8 +12,9 @@ import { useEffect, useState } from 'react';
 import { ChattingServiceKit } from '../../../SockJS/SockInstance';
 
 export const ChatRoom = () => {
-  const accessToken = localStorage.getItem('accessToken');
-  // console.log(accessToken);
+
+  const [chatLog, setChatLog] = useState([]);
+  const accessToken = localStorage.getItem("accessToken");
 
   const [chatLog, setChatLog] = useState([]);
 
@@ -23,50 +24,41 @@ export const ChatRoom = () => {
   const [message, setMessage] = useState('');
   const inputMessage = (e) => {
     setMessage(e.target.value);
-  };
-
+  }
   const onEnter = (e) => {
     if (e.keyCode === 13) {
-      // sendMessage();
-      console.log('----- 메시지 전송 성공! -----');
-    }
+      console.log("----- 메시지 전송 성공! -----")
+    };
   };
 
   ChattingServiceKit.onConnect('/topic/greetings', {}, (newMessage) => {
-    // setMessage(newMessage.content);
-
-    setReceiveMsg(newMessage.content);
+    setChatLog([
+      ...chatLog,
+      newMessage.content
+    ]);
+    console.log(newMessage.content);
   });
-
-  useEffect(() => {
-    setChatLog([...chatLog, receiveMsg]);
-  }, [receiveMsg]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    ChattingServiceKit.sendMessage({
-      content: message,
-      accesstoken: accessToken,
-    });
-    setMessage('');
+    ChattingServiceKit.sendMessage({ content: message, accesstoken: accessToken });
   };
-
-  // console.log(chatLog);
-  console.log(message);
-
+  
   return (
     <ChatContainer>
       <StChatBoxContainer>
-        {chatLog !== 0 &&
-          chatLog.map((val, index) => {
-            return <h3 key={index}>{val}</h3>;
-          })}
-
-        {message.length > 0 ? (
-          <StUpLoading>메시지 발신 중...</StUpLoading>
-        ) : (
-          <StDownLoading>메시지 수신 중...</StDownLoading>
-        )}
+        {
+          chatLog?.map((chat) => {
+            return (
+              <StChatBox key={ chat }>
+                <ChatMsg>{ chat }</ChatMsg>
+              </StChatBox>
+            )
+          })
+        }
+        {
+          message.length > 0 ? <StUpLoading>메시지 발신 중...</StUpLoading> : <StDownLoading>메시지 수신 중...</StDownLoading>
+        }
       </StChatBoxContainer>
       <form onSubmit={submitHandler}>
         <StChatInput
